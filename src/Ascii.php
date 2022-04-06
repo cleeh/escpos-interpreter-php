ad9d8963102ab78d81062773fd6318fe671ede3c
JS <khk37601@gmail.com>
2022-04-06 02:26:10 +0000

    Add control ascii for escpos

---
+++ src/Ascii.php
@@ -0,0 +1,177 @@
+<?php
+namespace Character\Ascii;
+/**
+ * this file is part of ascii code: PHP 
+ * This software is distributed under the terms of the MIT license. See LICENSE.md
+ * for details.
+ */
+class ControlAscii
+{
+    /**
+     * ASCII null control character
+     */
+    const NUL = "\x00";
+
+    /**
+     * ASCII start header control character
+     */
+    const SOH = "\x01";
+    
+    /**
+     * ASCII start Text control character
+     */
+    const STX = "\x02";
+    
+    /**
+     * ASCII end Text control character
+     */
+    const ETX = "\x03";
+    
+    /**
+     * ASCII end transmission control character
+     */
+    const EOT = "\x04";
+    
+    /**
+     * ASCII Enquiry control character
+     */
+    const ENQ = "\x05";
+    
+    /**
+     * ASCII Acknowledgment control character
+     */
+    const ACK = "\x06";
+    
+    /**
+     * ASCII Bell control character
+     */
+    const BEL = "\x07";
+    
+    /**
+     * ASCII Backspace control character
+     */
+    const BS = "\x08";
+   
+    /**
+     * ASCII Horizontal Tab control character
+     */
+    const HT = "\x09";
+   
+    /**
+     * ASCII Line feed Tab control character
+     */
+    const LF = "\x0A";
+   
+    /**
+     * ASCII Vertical Tab control character
+     */
+    const VT = "\x0B";
+  
+    /**
+     * ASCII Form feed control character
+     */
+    const FF = "\x0C";
+    
+    /**
+     * ASCII Carriage return control character
+     */
+    const CR = "\x0D";
+    
+    /**
+     * ASCII Shift Out control character
+     */
+    const SO = "\x0E";
+    
+    /**
+     * ASCII Shift In control character
+     */
+    const SI = "\x0F";
+    
+    /**
+     * ASCII Data Link Escape control character
+     */
+    const DLE = "\x10";
+    
+    /**
+     * ASCII Device Control 1 control character
+     */
+    const DC1 = "\x11";
+    
+    /**
+     * ASCII DDevice Control 2 control character
+     */
+    const DC2 = "\x12";
+    
+    /**
+     * ASCII DDevice Control 3 control character
+     */
+    const DC3 = "\x13";
+    
+    /**
+     * ASCII DDevice Control 4 control character
+     */
+    const DC4 = "\x14";
+    
+    /**
+     * ASCII Negative Acknowledgement control character
+     */
+    const NAK = "\x15";
+    
+    /**
+     * ASCII Synchronous idle control character
+     */
+    const SYN = "\x16";
+    
+    /**
+     * ASCII End of Transmission Block control character
+     */
+    const ETB = "\x17";
+
+    /**
+     * ASCII Cancel control character
+     */
+    const CAN = "\x18";
+    
+    /**
+     * ASCII End of Medium control character
+     */
+    const EM = "\x19";
+    
+    /**
+     * ASCII Substitute control character
+     */
+    const SUB = "\x1A";
+    
+    /**
+     * ASCII Escape control character
+     */
+    const ESC = "\x1B";
+    
+    /**
+     * ASCII File Separator control character
+     */
+    const FS = "\x1C";
+
+    /**
+     * ASCII Group Separator control character
+     */
+    const GS = "\x1D";
+
+    /**
+     * ASCII Record Separator control character
+     */
+    const RS = "\x1E";
+
+    /**
+     * ASCII Unit Separator control character
+     */
+    const US = "\x1F";
+    
+    /**
+     * ASCII DEL control character
+     */
+    const DEL = "\x7F";
+}
+
+
+
