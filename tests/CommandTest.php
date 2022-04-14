0d8a2283015c13d81b1745385ff67df7e036e49c
rockatoo <codemaniac73@gmail.com>
2022-04-14 05:15:03 +0000

    split data into determinant and params

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -6,44 +6,84 @@ use Command\Command;
 
 class CommandTest extends TestCase
 {
-  protected $arrayData;
-  protected $stringData;
+  protected $arrayDeterminant;
+  protected $stringDeterminant;
   protected $codeString;
 
   public function setUp(): void
   {
-    $this->arrayData = array(27, 33, 48);
+    $this->arrayDeterminant = [27, 33];
+    $this->arrayParams = [48];
+    $this->stringDeterminant = '!';
+    $this->stringParams = '0';
+
+    $this->arrayData = [27, 33, 48];
     $this->stringData = '!0';
     $this->codeString = 'ESC ! 0';
+    $this->hexString = '1b2130';
   }
 
   public function testInitializeCommand()
   {
-    $command = new Command($this->stringData);
-    $this->assertSame($this->arrayData, $command->toIntArray());
+    $command = new Command($this->stringDeterminant, $this->stringParams);
+    $this->assertSame($this->arrayDeterminant, $command->getDeterminant(), '<determinant> of command initialized with <string determinant & string params> is different from the expected value');
+    $this->assertSame($this->arrayParams, $command->getParams(), '<params> of command initialized with <string determinant & string params> is different from the expected value');
 
-    $command = new Command($this->arrayData);
-    $this->assertSame($this->arrayData, $command->toIntArray());
+    $command = new Command($this->stringDeterminant, $this->arrayParams);
+    $this->assertSame($this->arrayDeterminant, $command->getDeterminant(), '<determinant> of command initialized with <string determinant & array params> is different from the expected value');
+    $this->assertSame($this->arrayParams, $command->getParams(), '<params> of command initialized with <string determinant & array params> is different from the expected value');
+
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
+    $this->assertSame($this->arrayDeterminant, $command->getDeterminant(), '<determinant> of command initialized with <array determinant & string params> is different from the expected value');
+    $this->assertSame($this->arrayParams, $command->getParams(), '<params> of command initialized with <array determinant & string params> is different from the expected value');
+
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
+    $this->assertSame($this->arrayDeterminant, $command->getDeterminant(), '<determinant> of command initialized with <array determinant & array params> is different from the expected value');
+    $this->assertSame($this->arrayParams, $command->getParams(), '<params> of command initialized with <array determinant & array params> is different from the expected value');
+  }
+
+  public function testGetDeterminant()
+  {
+    $command = new Command($this->arrayDeterminant, []);
+    $this->assertSame($this->arrayDeterminant, $command->getDeterminant());
+  }
+
+  public function testGetDeterminantLength()
+  {
+    $command = new Command($this->arrayDeterminant, []);
+    $this->assertSame(count($this->arrayDeterminant), $command->getDeterminantLength());
+  }
+
+  public function testGetParams()
+  {
+    $command = new Command([], $this->arrayParams);
+    $this->assertSame($this->arrayParams, $command->getParams());
+  }
+
+  public function testGetParamsLength()
+  {
+    $command = new Command([], $this->arrayParams);
+    $this->assertSame(count($this->arrayParams), $command->getParamsLength());
   }
 
   public function testToIntArray()
   {
-    $command = new Command($this->arrayData);
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
     $this->assertSame($this->arrayData, $command->toIntArray());
   }
   public function testToString()
   {
-    $command = new Command($this->arrayData);
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
     $this->assertSame($this->stringData, $command->__toString());
   }
   public function testToHexString()
   {
-    $command = new Command($this->stringData);
-    $this->assertSame("1b2130", $command->toHexString());
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
+    $this->assertSame($this->hexString, $command->toHexString());
   }
   public function testToCodeString()
   {
-    $command = new Command($this->arrayData);
+    $command = new Command($this->arrayDeterminant, $this->arrayParams);
     $this->assertSame($this->codeString, $command->toCodeString());
   }
 }
