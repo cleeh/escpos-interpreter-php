10ac66656f911661e8d6010a806107a8620bd70a
JS <khk37601@gmail.com>
2022-04-14 08:22:40 +0000

    Update testcase in FileTest.php

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -6,15 +6,32 @@ use PHPUnit\Framework\TestCase;
 
 class FileTest extends TestCase
 {   
-    private $fileName = '../data/command.json';
+    private $testExistFileOfJson = 'resources/test.json';
+    private $testExistFileOfText = 'resources/test.txt';
+    private $testNotExistFileOfJson = 'resources/empty.json';
+    private $testNotExistFileOfText = 'resources/empty.txt';
+
 
     public function testReadJson()
     {   
-        $this->assertSame('array', gettype(File::readJson($this->fileName)));
+        $testContentOfJson = File::readJson($this->testExistFileOfJson);
+        $this->assertSame('array', gettype($testContentOfJson));
+        $this->assertSame('Initialize printer', $testContentOfJson[0]['description']);
+        $this->assertSame('ESC @', $testContentOfJson[0]['code']);
+        $this->assertSame('1b40', $testContentOfJson[0]['hex']);
+        $this->assertSame(array(27, 64), $testContentOfJson[0]['array']);
+
+        $testNotExistContentOfJson = File::readJson($this->testNotExistFileOfJson);
+        $this->assertSame(false, $testNotExistContentOfJson);
     }
 
     public function testReadAllText()
     {   
-        $this->assertSame('string', gettype(File::readAllText($this->fileName)));
+        $testContentOfText = File::readAllText($this->testExistFileOfText);
+        $this->assertSame('string', gettype($testContentOfText));
+        $this->assertSame('this is text', $testContentOfText);
+
+        $testNotExistContentOfText = File::readJson($this->testNotExistFileOfJson);
+        $this->assertSame(false, $testNotExistContentOfText);
     }
 }
